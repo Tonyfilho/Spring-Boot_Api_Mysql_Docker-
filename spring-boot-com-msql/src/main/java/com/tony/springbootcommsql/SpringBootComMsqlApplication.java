4ebe6143b789614117037de5e830613621cadf74
tony filho <antonio.m.filho>
2021-04-10 12:52:26 +0100

    1º commit

---
+++ spring-boot-com-msql/src/main/java/com/tony/springbootcommsql/SpringBootComMsqlApplication.java
@@ -0,0 +1,13 @@
+package com.tony.springbootcommsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootComMsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootComMsqlApplication.class, args);
+	}
+
+}
---
+++ spring-boot-com-msql/src/test/java/com/tony/springbootcommsql/SpringBootComMsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tony.springbootcommsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootComMsqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
